a3cc1d4a96d8bd639c02936815a187505264a40e
San Kim <waterfog9580@gmail.com>
2023-08-31 20:26:33 +0900

    :sparkles: Add new Coroutine vs Routine tutorial
    A new tutorial detailing the use and differences between coroutines and routines was added to the project. This provides concrete examples in Kotlin on how to write and use coroutines for asynchronous programming. It serves as a useful guide for developers who are new to coroutines.

---
+++ src/main/kotlin/01_routine_corutine/main.kt
@@ -0,0 +1,28 @@
+package `01_routine_corutine`
+
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.yield
+
+
+fun main() = runBlocking {
+    printWithThread("start")
+    launch {
+        coroutine(1, 2)
+    }
+    yield()
+    printWithThread("end")
+}
+
+suspend fun coroutine(
+    num1: Int,
+    num2: Int
+) {
+    val result = num1 + num2
+    yield()
+    printWithThread(result.toString())
+}
+
+fun printWithThread(value: String) {
+    println("[${Thread.currentThread().name}] : $value")
+}
